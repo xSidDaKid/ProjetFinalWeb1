fc29fa9deeed4510ce0d4bf1e66359f6dfdd5510
Shajaan Balasingam <sbalasingam@dept-info.crosemont.quebec>
2021-04-13 17:30:38 +0000

    Création des différents tables

--- sql/creer_db_tables.sql
+++ sql/creer_db_tables.sql
@@ -1,3 +1,18 @@
 CREATE DATABASE cinema DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;
 
 USE cinema
+
+CREATE TABLE utilisateur (
+  mot_passe         VARCHAR(255),
+  categorie         VARCHAR(255),
+);
+CREATE TABLE acheteur (
+    
+  nom               VARCHAR(255),
+  telephone         VARCHAR(255),
+  mot_passe         DECIMAL(8, 2),
+
+);
+CREATE TABLE infos_Cinema ();
+CREATE TABLE tab_Cinema ();
+CREATE TABLE billet ();
