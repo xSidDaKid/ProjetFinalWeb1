1473188d52510f06527ebfeb6542551361b8e04f
Shajaan Balasingam <sbalasingam@dept-info.crosemont.quebec>
2021-04-13 18:13:45 +0000

    Création du fichier pour supprimer la Base de Données

---
+++ sql/supprimer_tables_db.sql
@@ -0,0 +1,19 @@
+USE cinema;
+
+DELETE FROM utilisateur;
+DROP TABLE utilisateur;
+
+DELETE FROM acheteur;
+DROP TABLE acheteur;
+
+DELETE FROM infos_Cinema;
+DROP TABLE infos_Cinema;
+
+DELETE FROM tab_Cinema;
+DROP TABLE tab_Cinema;
+
+DELETE FROM billet;
+DROP TABLE billet;
+
+
+DROP DATABASE cinema;
