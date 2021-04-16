a27469c1225dd81fd5f1ab2b15b6aeb79e278e4e
Shajaan Balasingam <sbalasingam@dept-info.crosemont.quebec>
2021-04-16 15:29:13 +0000

    Ajout des attributs, constructeur, accesseurs, mutateurs, méthodes et toString

--- modele/Acheteur.class.php
+++ modele/Acheteur.class.php
@@ -1,1 +1,45 @@
+<?php
+class Acheteur {
+    // Attributs
+    private $idAcheteur;//int
+    private $nom;//String
+    private $telephone;//String
+    private $solde;//Float
+    private $lesBillets = [];//Array <Billet>
 
+    //Constructeur
+    public function __construct($unId, $unNom, $unTelephone, $unSolde){
+        $this->idAcheteur = $unId;
+        $this->nom = $unNom;
+        $this->telephone = $unTelephone;
+        $this->solde = $unSolde;
+    }
+
+    public function getIdAcheteur() { return $this->idAcheteur; }
+    public function getNom() { return $this->nom; }
+    public function getTelephone() { return $this->telephone; }
+    public function getSolde() { return $this->solde; }
+    public function getLesBillets() { return $this->lesBillets; }
+    
+    public function setTelephone() { $this->telephone = $unTelephone; }
+
+    public function ajouterBillet($nouveauBillet){
+
+    }
+
+    public function chargerSolde($montant){
+
+    }
+
+    public function payerSolde($montant){
+
+    }
+
+    public function __toString() {
+       $message = "[#" .$this->idAcheteur. "] ".$this->nom.", Telephone: " .$this->telephone. ", Solde: " .$this->solde;
+       foreach ($lesBillets as $unBillet) {
+			echo "Billet: " .$unBillet;
+		}       
+    }
+}
+?>
