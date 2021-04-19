0009f115b8c4a9f95bf1cfbe7722d2d2b953dc71
Shajaan Balasingam <shajaan_71@hotmail.com>
2021-04-19 18:20:45 +0000

    Changement de l'ordre

--- sql/supprimer_tables_db.sql
+++ sql/supprimer_tables_db.sql
@@ -1,5 +1,13 @@
-DELETE FROM utilisateur;
-DROP TABLE utilisateur;
+#
+# Description : Script pour supprimer la base de données.
+#               
+# Date        : 2021/04/13
+# Auteurs     : Kumaran Satkunanathan
+#               Louai Roueha
+#               Shajaan Balasingam
+#
+
+USE cinema;
 
 DELETE FROM billet;
 DROP TABLE billet;
@@ -7,12 +15,13 @@ DROP TABLE billet;
 DELETE FROM acheteur;
 DROP TABLE acheteur;
 
+DELETE FROM utilisateur;
+DROP TABLE utilisateur;
 
 DELETE FROM tab_Cinema;
 DROP TABLE tab_Cinema;
 
 DELETE FROM infos_Cinema;
 DROP TABLE infos_Cinema;
-
 
 DROP DATABASE cinema;
