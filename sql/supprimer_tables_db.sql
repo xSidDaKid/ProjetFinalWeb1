d058450d4e72a7721abc72711238fe38394f3b81
Shajaan Balasingam <shajaan_71@hotmail.com>
2021-04-19 17:42:16 +0000

    Changement de l'ordre pour supprimer

--- sql/supprimer_tables_db.sql
+++ sql/supprimer_tables_db.sql
@@ -1,19 +1,18 @@
-USE cinema;
-
 DELETE FROM utilisateur;
 DROP TABLE utilisateur;
 
-DELETE FROM infos_Cinema;
-DROP TABLE infos_Cinema;
+DELETE FROM billet;
+DROP TABLE billet;
 
 DELETE FROM acheteur;
 DROP TABLE acheteur;
 
+
 DELETE FROM tab_Cinema;
 DROP TABLE tab_Cinema;
 
-DELETE FROM billet;
-DROP TABLE billet;
+DELETE FROM infos_Cinema;
+DROP TABLE infos_Cinema;
 
 
 DROP DATABASE cinema;
