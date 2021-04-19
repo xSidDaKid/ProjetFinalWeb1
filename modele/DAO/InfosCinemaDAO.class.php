baccc48892ab2d07f62143c27c004461be9de429
Shajaan Balasingam <sbalasingam@dept-info.crosemont.quebec>
2021-04-19 16:28:41 +0000

    Ajout de la classe InfosCinemaDAO

---
+++ modele/DAO/InfosCinemaDAO.class.php
@@ -0,0 +1,25 @@
+<?php
+
+/*
+#
+# Description : Classe InfoCinemaDAO
+#               
+# Date        : 2021/04/16
+# Auteurs     : Kumaran Satkunanathan
+#               Louai Roueha
+#               Shajaan Balasingam
+#
+*/
+
+// ****** INLCUSIONS *******
+// si la constante n'existe pas, on la crée
+if (defined("DOSSIER_BASE_INCLUDE") == false) {
+	$chemin=(substr($_SERVER['DOCUMENT_ROOT'],-1)=="/")?$_SERVER['DOCUMENT_ROOT']:$_SERVER['DOCUMENT_ROOT']."/";
+	define("DOSSIER_BASE_INCLUDE", $chemin."projet_h2021_g16/");
+}
+include_once(DOSSIER_BASE_INCLUDE."modele/Cinema.class.php"); 
+
+class InfosCinemaDAO implements DAO {
+
+}
+?>
