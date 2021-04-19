8a236605b194c68a09aa774c885c48ee42274842
Shajaan Balasingam <sbalasingam@dept-info.crosemont.quebec>
2021-04-19 16:42:56 +0000

    Ajout de la méthode chercher. TODO: Vérifier si ca marche

--- modele/DAO/AcheteurDAO.class.php
+++ modele/DAO/AcheteurDAO.class.php
@@ -17,7 +17,33 @@ if (defined("DOSSIER_BASE_INCLUDE") == false) {
 include_once(DOSSIER_BASE_INCLUDE."modele/Billet.class.php"); 
 class AcheteurDAO implements DAO {	
 
-
+	public static function chercher($unNumero) { 
+			// obtenir la connexion
+			try {
+				$connexion=ConnexionBD::getInstance();
+			} catch (Exception $e) {
+				throw new Exception("Impossible d’obtenir la connexion à la BD."); 
+			}
+			// valeur par défaut pour la variable à retourner si non-trouvée
+			$unAcheteur=null;
+			
+			// Préparer et exécute une requête de type DAOStatement avec des paramètres SQL	
+			$requete=$connexion->prepare("SELECT * FROM district WHERE nom=?");
+			$requete->execute(array($unNumero));
+			// Analyser l’enregistrement, s’il existe,
+			if ($requete->rowCount()!=0) {
+				// ... et créer l’instance de l'Acheteur
+				$rangee=$requete->fetch();
+				$unAcheteur = new Acheteur($rangee['id_acheteur'], $rangee['nom'], $rangee['telephone'], $rangee['solde']);
+               
+				}
+			}
+			
+			// fermer le curseur de la requête et la connexion à la BD
+			$requete->closeCursor();
+			ConnexionBD::close();
+			return $unDistrict;
+		} 
 }
 
 ?>
