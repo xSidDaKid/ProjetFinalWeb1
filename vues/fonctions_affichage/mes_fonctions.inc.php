43c6c404760c9d77b1ba14ea6808602857df7e9b
Kumaran Satkunanathan <kumarankulam@hotmail.com>
2021-04-26 16:08:20 +0000

    Update mes_fonctions.inc.php

--- vues/fonctions_affichage/mes_fonctions.inc.php
+++ vues/fonctions_affichage/mes_fonctions.inc.php
@@ -10,6 +10,18 @@
 		Fonctions d'affichage des options du menu
 	********************************************** */
 	function afficherMenu($categorieActeur) {
+
+	/*	$tableauOptions=[ "Accueil"=>DOSSIER_BASE_LIENS."/index.php?=voirPageAccueil",
+						  "Affichage de tous les Candidats"=>DOSSIER_BASE_LIENS."/index.php?action=voirLesCandidats",
+						  "Recherche d'un District"=>DOSSIER_BASE_LIENS."/index.php?action=chercherUnDistrict",
+						  "Collège Rosemont"=>"https://www.crosemont.qc.ca/",
+						  "W3 schools"=>"https://www.w3schools.com/"];
+		foreach ($tableauOptions as $option => $hyperlien) {
+			echo "<div>";
+			echo "<a href='$hyperlien'>$option</a>";  
+			echo "</div>";
+		}
+	}
 	}
 	/* *******************************************
 		Fonctions d'affichage des messages d'erreurs
