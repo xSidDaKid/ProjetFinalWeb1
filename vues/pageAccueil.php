1465278e567b5e4ec3171a7cbf4acd7bfc606fd7
Shajaan <shajaan_71@hotmail.com>
2021-05-18 15:44:44 -0400

    Création de la page d'accueil

--- vues/pageAccueil.php
+++ vues/pageAccueil.php
@@ -10,18 +10,23 @@
     <?php
 		include(DOSSIER_BASE_INCLUDE."vues/inclusions_html/head.inc.php");
 	?>
-
+    <link rel="stylesheet" href="<?php echo DOSSIER_BASE_LIENS;?>/css/style.css" />
 </head>
 
 <body>
-
     <!-- MENU -->
     <?php
 		include (DOSSIER_BASE_INCLUDE."vues/inclusions_html/menu.inc.php");
 	?>
-    <h1>Voci notre page d'accueil</h1>
+
+    <div class="bg a">
+        <div class="a pt-5 pb-5">
+            <h1 class="pt-5 pb-5">Voici notre page d'accueil</h1>
+        </div>
+    </div>
     <hr />
-    <h5><a href="<?php echo DOSSIER_BASE_LIENS?>/modele" target="_blank">liens vers les dossiers du modele</a></h5>
+    <h5><a href="<?php echo DOSSIER_BASE_LIENS?>/modele" target="_blank">liens vers les dossiers du modele</a>
+    </h5>
 </body>
 
 </html>
