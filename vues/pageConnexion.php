4b9d1bc5376e3ced9c517cef66cfd3ce7c3b0cf9
Shajaan <shajaan_71@hotmail.com>
2021-05-18 17:34:45 -0400

    Modification de la page de page de connexion

--- vues/pageConnexion.php
+++ vues/pageConnexion.php
@@ -10,6 +10,7 @@
     <?php
 		include(DOSSIER_BASE_INCLUDE."vues/inclusions_html/head.inc.php");
 	?>
+    <link rel="stylesheet" href="<?php echo DOSSIER_BASE_LIENS;?>/css/style.css" />
 
 </head>
 
@@ -22,24 +23,24 @@
     <!-- SE CONNECTER -->
     <div class="container">
         <div class="row justify-content-center">
-            <div class="col-md-5 pt-5">
+            <div class="col-md-5 pt-6">
                 <div class="card">
 
-                    <div class="card-header">Connexion</div>
+                    <div class="card-header connexion">Connexion</div>
 
                     <div class="card-body">
                         <form action="<?php echo DOSSIER_BASE_LIENS;?>/index.php?action=seConnecter" method="post">
                             <input type="hidden" name="formulaireConnexion" value="true" />
                             <div class="form-group row">
-                                <label class="pr-4">Nom d'utilisateur : </label>
+                                <label class="pr-4 connexion">Nom d'utilisateur : </label>
                                 <input class="pr-5" type="text" name="id_utilisateur" />
                             </div>
                             <div class="form-group row">
-                                <label class="pr-5">Mot de passe : </label>
+                                <label class="pr-5 connexion">Mot de passe : </label>
                                 <input class="pr-5" type="text" name="mot_passe" />
                             </div>
                             <div>
-                                <input class="ml-0" type="submit" value="Soumettre" />
+                                <input type="submit" value="Soumettre" />
                             </div>
                         </form>
                     </div>
