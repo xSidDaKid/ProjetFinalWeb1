dceaeff95fe076f2c3c099c0fafccbcd7cc24b1a
Shajaan <shajaan_71@hotmail.com>
2021-05-18 18:44:37 -0400

    Mis à l'ordre pour afficher les menus corrects pour chaque type d'utilisateur

--- vues/fonctions_affichage/mes_fonctions.inc.php
+++ vues/fonctions_affichage/mes_fonctions.inc.php
@@ -11,45 +11,29 @@
 	********************************************** */
 	function afficherMenu($categorieActeur) {
 
-			/*$tableauOptions=[ "Accueil"=>DOSSIER_BASE_LIENS."/index.php?=voirPageAccueil",
-				"Informations générales"=>DOSSIER_BASE_LIENS."/index.php?action=voirPageInfo",
-				"Événements"=>DOSSIER_BASE_LIENS."/index.php?action=voirPageEvenement",
-				"Connexion"=>DOSSIER_BASE_LIENS."/index.php?action=seConnecter", 
-				"Création d'un compte"=>DOSSIER_BASE_LIENS."/index.php?action=voirPageCreationCompte"];
-			*/
-			$tableauOptions=[ "Accueil"=>DOSSIER_BASE_LIENS."/index.php?=voirPageAccueil",
-				"Informations générales"=>DOSSIER_BASE_LIENS."/index.php?action=voirPageInfo",
-				"Événements"=>DOSSIER_BASE_LIENS."/index.php?action=voirPageEvenement",
-				"Connexion"=>DOSSIER_BASE_LIENS."/index.php?action=seConnecter", 
-				"Création d'un compte"=>DOSSIER_BASE_LIENS."/index.php?action=voirPageCreationCompte",
-				"Modification du compte"=>DOSSIER_BASE_LIENS."/index.php?action=voirPageModificationCompte",
-				"Achat de billet"=>DOSSIER_BASE_LIENS."/index.php?action=voirPageAchatBillet",
-				"Ajout de représentations"=>DOSSIER_BASE_LIENS."/index.php?action=voirPageAjoutRepresentations",
-				"Supression d'acheteurs"=>DOSSIER_BASE_LIENS."/index.php?action=voirPageSupressionAcheteurs"];
-  
+		//Visiteur par défaut
+		$tableauOptions=[ "Accueil"=>DOSSIER_BASE_LIENS."/index.php?=voirPageAccueil",
+			"Informations générales"=>DOSSIER_BASE_LIENS."/index.php?action=voirPageInfo",
+			"Événements"=>DOSSIER_BASE_LIENS."/index.php?action=voirPageEvenement",
+			"Connexion"=>DOSSIER_BASE_LIENS."/index.php?action=seConnecter", 
+			"Création d'un compte"=>DOSSIER_BASE_LIENS."/index.php?action=voirPageCreationCompte"];
+
 		if ($categorieActeur == "acheteur") {
 			$tableauOptions=[ "Accueil"=>DOSSIER_BASE_LIENS."/index.php?=voirPageAccueil",
 				"Informations générales"=>DOSSIER_BASE_LIENS."/index.php?action=voirPageInfo",
 				"Événements"=>DOSSIER_BASE_LIENS."/index.php?action=voirPageEvenement",
-				"Connexion"=>DOSSIER_BASE_LIENS."/index.php?action=seConnecter", 
-				"Création d'un compte"=>DOSSIER_BASE_LIENS."/index.php?=voirPageAccueil", 
-				"Déconnexion"=>DOSSIER_BASE_LIENS."/index.php?=voirPageAccueil", 
-				"Modification du compte"=>DOSSIER_BASE_LIENS."/index.php?=voirPageAccueil",
-				"Achat de billet"=>DOSSIER_BASE_LIENS."/index.php?=voirPageAccueil"];
-			/*array_push($tableauOptions, "Déconnexion"=>DOSSIER_BASE_LIENS."/index.php?=voirPageAccueil", 
-										"Modification du compte"=>DOSSIER_BASE_LIENS."/index.php?=voirPageAccueil",
-										"Achat de billet"=>DOSSIER_BASE_LIENS."/index.php?=voirPageAccueil");*/
+				"Déconnexion"=>DOSSIER_BASE_LIENS."/index.php?=voirPageAccueil", //À FAIRE <----------------------------------------------------------
+				"Modification du compte"=>DOSSIER_BASE_LIENS."/index.php?action=voirPageModificationCompte",
+				"Achat de billet"=>DOSSIER_BASE_LIENS."/index.php?action=voirPageAchatBillet"];
 		}
+		
 		if ($categorieActeur == "administrateur") {
 			$tableauOptions=[ "Accueil"=>DOSSIER_BASE_LIENS."/index.php?=voirPageAccueil",
 				"Informations générales"=>DOSSIER_BASE_LIENS."/index.php?action=voirPageInfo",
 				"Événements"=>DOSSIER_BASE_LIENS."/index.php?action=voirPageEvenement", 
-				"Déconnexion"=>DOSSIER_BASE_LIENS."/index.php?=voirPageAccueil", 
-				"Supression d'acheteurs"=>DOSSIER_BASE_LIENS."/index.php?=voirPageAccueil",
+				"Déconnexion"=>DOSSIER_BASE_LIENS."/index.php?=voirPageAccueil", //À FAIRE <----------------------------------------------------------
+				"Supression d'acheteurs"=>DOSSIER_BASE_LIENS."/index.php?action=voirPageSupressionAcheteurs",
 				"Ajout de représentations"=>DOSSIER_BASE_LIENS."/index.php?action=voirPageAjoutRepresentations"];
-			/*array_push($tableauOptions, "Déconnexion"=>DOSSIER_BASE_LIENS."/index.php?=voirPageAccueil", 
-										"Supression d'acheteurs"=>DOSSIER_BASE_LIENS."/index.php?=voirPageAccueil",
-										"Ajout de représentations"=>DOSSIER_BASE_LIENS."/index.php?=voirPageAccueil");*/
 		}
 		
 
