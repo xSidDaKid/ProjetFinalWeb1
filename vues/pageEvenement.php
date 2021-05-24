2991a0eca7b765e70b0fc8dd5b26a8f25cc37dbd
ShishTaoukGainz <louai.louairoueha@gmail.com>
2021-05-24 18:08:10 +0000

    Update pageEvenement.php

--- vues/pageEvenement.php
+++ vues/pageEvenement.php
@@ -11,6 +11,7 @@
     <title>Projet-test</title>
     <?php
 		include(DOSSIER_BASE_INCLUDE."vues/inclusions_html/head.inc.php");
+		
 	?>
     <link rel="stylesheet" href="<?php echo DOSSIER_BASE_LIENS;?>/css/style.css" />
 </head>
@@ -20,7 +21,19 @@
     <?php
 	    include (DOSSIER_BASE_INCLUDE."vues/inclusions_html/menu.inc.php");
 	?>
-    <h1>Événement</h1>
+    <h1>Voici la page d'événement</h1>
+	<form class="mon_formulaire" action="<?php echo DOSSIER_BASE_LIENS;?>/index.php?action=voirPageEvenement" method="post">
+						<div>
+							<label>Code infos du cinéma : </label>
+							<input type="text" name="code_infos"/>
+						</div>
+						<input type="submit" value="Chercher le code infos"/>
+					</form>
+	<?php				
+	$unCinema=$controleur->getLeCode();
+	echo $unCinema;
+	?>
+	
 </body>
 
 </html>
