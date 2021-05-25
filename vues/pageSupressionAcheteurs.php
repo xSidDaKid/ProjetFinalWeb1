55603b7a7bc4193c9b2af8069b97eddfeabbefd9
Shajaan <shajaan_71@hotmail.com>
2021-05-25 17:27:49 -0400

    Ajout de Bootstrap pour la vue

--- vues/pageSupressionAcheteurs.php
+++ vues/pageSupressionAcheteurs.php
@@ -19,31 +19,33 @@
     <?php
 	    include (DOSSIER_BASE_INCLUDE."vues/inclusions_html/menu.inc.php");
 	?>
-    <h1>Voici la page de suppression d'un acheteur</h1>
-    <?php
-          echo "<div class='bg-success text-center'>";
-          afficherSucces($controleur->getMessagesSucces());
-          echo "</div>";
-          echo "<div class='bg-danger text-center'>";
-          afficherErreurs($controleur->getMessagesErreur());
-          echo "</div>";
-    ?>
+    <h1>Suppression d'un acheteur</h1>
+    <br>
+    <div class="card w-50 ml-3">
+        <?php
+            echo "<div class='bg-success text-center'>";
+            afficherSucces($controleur->getMessagesSucces());
+            echo "</div>";
+            echo "<div class='bg-danger text-center'>";
+            afficherErreurs($controleur->getMessagesErreur());
+            echo "</div>";
+            $tabAcheteurs = $controleur->getAcheteurs();
+            echo "<ul class='list-group'>";
+            foreach ($tabAcheteurs as $tab) {
+                echo "<li class='list-group-item'>";
+                echo $tab;
+                echo "</li>";
+            }
+            echo "</ul>";
+        ?>
 
-    <?php
-       $tabAcheteurs = $controleur->getAcheteurs();
-       echo "<ul class='list-group'>";
-       foreach ($tabAcheteurs as $tab) {
-           echo "<li class='list-group-item'>";
-           echo $tab;
-           echo "</li>";
-        }
-        echo "</ul>";
-    ?>
-    <div class="form-group">
         <form action="<?php echo DOSSIER_BASE_LIENS;?>/index.php?action=voirPageSupressionAcheteurs" method="post">
-            <label for="text">ID Acheteur</label>
-            <input type="number" name="id_acheteur" id="" class="form-control" placeholder="[#ID]"
-                aria-describedby="helpId">
+            <div class="form-group">
+                <label for="text">ID Acheteur</label>
+                <input type="number" name="id_acheteur" id="" class="form-control" placeholder="[#ID]"
+                    aria-describedby="helpId">
+            </div>
+            <button type="submit" class="btn btn-primary">Submit</button>
         </form>
     </div>
 </body>
