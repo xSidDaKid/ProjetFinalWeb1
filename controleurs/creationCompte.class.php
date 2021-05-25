c99728a4d013fb39711bac8f56d82cb2592253c0
ShishTaoukGainz <louai.louairoueha@gmail.com>
2021-05-25 22:15:30 +0000

    Update creationCompte.class.php

--- controleurs/creationCompte.class.php
+++ controleurs/creationCompte.class.php
@@ -1,24 +1,65 @@
-<?php
-// *****************************************************************************************
-// Description   : Contrôleur pour la page d'info
-// Date          : 18 mai 2021
-// Auteur        : Shajaan Balasingam
-// Modifé par    : 
-// *****************************************************************************************
-include_once(DOSSIER_BASE_INCLUDE."controleurs/controleur.abstract.class.php");
-include_once(DOSSIER_BASE_INCLUDE."modele/DAO/InfosCinemaDAO.class.php");
+<!--
+  Projet  H2021 
+  Noms    : Kumaran Satkunanathan
+            Louai Roueha
+            Shajaan Balasingam
+-->
+<!DOCTYPE html>
+<html lang="fr">
 
-class creationCompte extends Controleur {
+<head>
+    <title>Projet-test</title>
+    <?php
+		include(DOSSIER_BASE_INCLUDE."vues/inclusions_html/head.inc.php");
+	?>
+</head>
 
-    // ******************* Constructeur vide
-	public function __construct() {
-		parent::__construct();
-	}
+<body>
+    <!-- MENU -->
+    <?php
+	    include (DOSSIER_BASE_INCLUDE."vues/inclusions_html/menu.inc.php");
+	?>
+    <h1>Création d'un compte</h1>
+   
+    <form class="mon_formulaire" action="<?php echo DOSSIER_BASE_LIENS;?>/index.php?action=voirPageCreationCompte" method="post">
+						<div>
+							<label>Nom: </label>
+							<input type="text" name="nom" id="nom" required/>
+						</div>
+                        <div>
+							<label>Mot de passe : </label>
+							<input type="password" name="modeDePasse" id="modeDePasse" required/>
+						</div>
+                        <div>
+							<label>Téléphone : </label>
+							<input type="number" name="telephone" id="telephone" required/>
+						</div>
+						<input type="submit" value="Créer"/>
+					</form>   
 
-    // ******************* Méthode exécuter action
-    public function executerAction() {
-        return "pageCreationCompte";
-    }
-}
+     <?php
+          echo "<div class='bg-success text-center'>";
+          afficherSucces($controleur->getMessagesSucces());
+          echo "</div>";
+          echo "<div class='bg-danger text-center'>";
+          afficherErreurs($controleur->getMessagesErreur());
+          echo "</div>";
+    ?>
+   
+     <?php 
+       $tabAcheteurs = $controleur->getAcheteurs();
+       echo "<ul class='list-group'>";
+       foreach ($tabAcheteurs as $tab) {
+           echo "<li class='list-group-item'>";
+           echo $tab;
+           echo "</li>";
+        }
+        echo "</ul>";
+        
+    ?>
+    
 
-?>
+
+</body>
+
+</html>
