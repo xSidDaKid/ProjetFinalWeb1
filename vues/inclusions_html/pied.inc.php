5327c78875dc1d37b83789e7bb89694d41e9619e
Shajaan <shajaan_71@hotmail.com>
2021-05-26 17:48:08 -0400

    Ajout du fichier pour le pied de page

---
+++ vues/inclusions_html/pied.inc.php
@@ -0,0 +1,10 @@
+<footer class="bg-dark text-white text-center text-lg-start fixed-bottom mt-4">
+    <!-- Copyright -->
+    <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
+        Ce site est créé par <b>Shajaan B., Louai R. et Kumaran S.</b>
+        <br>
+
+        © projet_h2021_g16
+    </div>
+    <!-- Copyright -->
+</footer>
