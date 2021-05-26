2e5f9597fd1bdca306842816ca7a83fee330da0f
Shajaan <shajaan_71@hotmail.com>
2021-05-25 23:04:56 -0400

    Modification de l'apparence de la page d'événement

--- vues/pageEvenement.php
+++ vues/pageEvenement.php
@@ -8,7 +8,7 @@
 <html lang="fr">
 
 <head>
-    <title>Projet-test</title>
+    <title>Événement</title>
     <?php
 		include(DOSSIER_BASE_INCLUDE."vues/inclusions_html/head.inc.php");
 		
@@ -21,63 +21,66 @@
     <?php
 	    include (DOSSIER_BASE_INCLUDE."vues/inclusions_html/menu.inc.php");
 	?>
-    <h1>Voici la page d'événement</h1>
-	
-	</br></br>
-	<form class="mon_formulaire" action="<?php echo DOSSIER_BASE_LIENS;?>/index.php?action=voirPageEvenement" method="post">
-						<div>
-							<label>Rechercher par Titre : </label>
-							<input type="text" name="titre"/>
-						</div>
-						<input type="submit" value="Chercher le titre"/>
-					</form>
-	<?php				
-	$unCinema=$controleur->getLeTitre();
-	echo $unCinema;
-	?>
-	
-	</br></br>
-	<form class="mon_formulaire" action="<?php echo DOSSIER_BASE_LIENS;?>/index.php?action=voirPageEvenement" method="post">
-						<div>
-							<label>Rechercher par date : </label>
-							<input type="text" name="date"/>
-						</div>
-						<input type="submit" value="Chercher par date"/>
-					</form>
-	<?php				
-	$unCinema=$controleur->getLaDate();
-	echo $unCinema;
-	?>
-	
-	</br></br>
-	<form class="mon_formulaire" action="<?php echo DOSSIER_BASE_LIENS;?>/index.php?action=voirPageEvenement" method="post">
-						<div>
-							<label>Rechercher par code : </label>
-							<input type="text" name="code_infos"/>
-						</div>
-						<input type="submit" value="Chercher le code infos"/>
-					</form>
-	<?php				
-	$unCinema=$controleur->getLeCode();
-	echo $unCinema;
-	?>
-	
-	</br></br>
-	<form class="mon_formulaire" action="<?php echo DOSSIER_BASE_LIENS;?>/index.php?action=voirPageEvenement" method="post">
-						<div>
-							<label>Rechercher par salle : </label>
-							<input type="text" name="salle"/>
-						</div>
-						<input type="submit" value="Chercher par salle"/>
-					</form>
-	<?php				
-	$unCinema=$controleur->getLaSalle();
-	echo $unCinema;
-	?>
+    <h1 class="p-3 display-2">Événement</h1>
+    </br>
+    <div class="card m-3 p-3 w-50">
+        <form class="mon_formulaire p-3" action="<?php echo DOSSIER_BASE_LIENS;?>/index.php?action=voirPageEvenement"
+            method="post">
+            <div>
+                <label>Rechercher par Titre : </label>
+                <input type="text" name="titre" />
+            </div>
+            <input type="submit" value="Chercher le titre" />
+        </form>
+        <?php				
+			$unCinema=$controleur->getLeTitre();
+			echo "<p class='lead p-2'>".$unCinema."</p>";
+		?>
 
-	
+        <form class="mon_formulaire p-3 m-2"
+            action="<?php echo DOSSIER_BASE_LIENS;?>/index.php?action=voirPageEvenement" method="post">
+            <div>
+                <label>Rechercher par date : </label>
+                <input type="text" name="date" />
+            </div>
+            <input type="submit" value="Chercher par date" />
+        </form>
+        <?php				
+			$unCinema=$controleur->getLaDate();
+			echo "<p class='lead p-2'>".$unCinema."</p>";
+		?>
 
-	
+        </br></br>
+        <form class="mon_formulaire p-3 m-2"
+            action="<?php echo DOSSIER_BASE_LIENS;?>/index.php?action=voirPageEvenement" method="post">
+            <div>
+                <label>Rechercher par code : </label>
+                <input type="text" name="code_infos" />
+            </div>
+            <input type="submit" value="Chercher le code infos" />
+        </form>
+        <?php				
+			$unCinema=$controleur->getLeCode();
+			echo "<p class='lead p-2'>".$unCinema."</p>";
+		?>
+
+        </br></br>
+        <form class="mon_formulaire p-3 m-2"
+            action="<?php echo DOSSIER_BASE_LIENS;?>/index.php?action=voirPageEvenement" method="post">
+            <div>
+                <label>Rechercher par salle : </label>
+                <input type="text" name="salle" />
+            </div>
+            <input type="submit" value="Chercher par salle" />
+        </form>
+        <?php				
+			$unCinema=$controleur->getLaSalle();
+			echo "<p class='lead p-2'>".$unCinema."</p>";
+		?>
+
+
+    </div>
+
 </body>
 
 </html>
