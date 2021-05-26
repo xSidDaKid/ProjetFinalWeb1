54919dbd99354e8ef1c1ad9131fcb3e552ffec6f
Shajaan <shajaan_71@hotmail.com>
2021-05-26 16:43:14 -0400

    Modification de la page d'acceuil

--- vues/pageAccueil.php
+++ vues/pageAccueil.php
@@ -8,7 +8,7 @@
 <html lang="fr">
 
 <head>
-    <title>Projet-Accueil</title>
+    <title>Page d'acceuil du cinéma</title>
     <?php
 		include(DOSSIER_BASE_INCLUDE."vues/inclusions_html/head.inc.php");
 	?>
@@ -26,12 +26,44 @@
           echo "</div>";
     ?>
 
-    <div class="container section-1">
+    <div class="container section-1 mt-2">
         Bonjour au Centre de Billeterie!
     </div>
-    <!--    <hr />
-    <h5><a href="<?php echo DOSSIER_BASE_LIENS?>/modele" target="_blank">liens vers les dossiers du modele</a>
-    </h5> -->
+    <br>
+    <h1 class="p-3 display-2">Voici nos films à l'affichage</h1>
+    <div class=" caroussel centrer w-50">
+        <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+            <ol class="carousel-indicators">
+                <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="3"></li>
+            </ol>
+            <div class="carousel-inner cycle">
+                <div class="carousel-item active">
+                    <img class="d-block w-100 image" src="images\photo.png" alt="First slide">
+                </div>
+                <div class="carousel-item">
+                    <img class="d-block w-100 image" src="images\photo1.png" alt="Second slide">
+                </div>
+                <div class="carousel-item">
+                    <img class="d-block w-100 image" src="images\photo2.png" alt="Third slide">
+                </div>
+                <div class="carousel-item">
+                    <img class="d-block w-100 image" src="images\photo3.png" alt="Third slide">
+                </div>
+            </div>
+            <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="sr-only">Previous</span>
+            </a>
+            <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="sr-only">Next</span>
+            </a>
+        </div>
+    </div>
+
 </body>
 
 </html>
