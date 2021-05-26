81cb84018ff3b80daff34ef211359b196839d9c2
ShishTaoukGainz <louai.louairoueha@gmail.com>
2021-05-26 18:30:50 +0000

    Update pageAjoutRepresentations.php

--- vues/pageAjoutRepresentations.php
+++ vues/pageAjoutRepresentations.php
@@ -8,7 +8,7 @@
 <html lang="fr">
 
 <head>
-    <title>Projet-test</title>
+    <title>pageAjoutRepresentations</title>
     <?php
 		include(DOSSIER_BASE_INCLUDE."vues/inclusions_html/head.inc.php");
 	?>
@@ -20,6 +20,22 @@
 	    include (DOSSIER_BASE_INCLUDE."vues/inclusions_html/menu.inc.php");
 	?>
     <h1>Voici la page d'ajout de représentations</h1>
+    <?php
+                        echo "<div class='bg-danger text-center'>";
+                        afficherErreurs($controleur->getMessagesErreur());
+                        echo "</div>";
+                    ?>
+
+    <form class="mon_formulaire" action="<?php echo DOSSIER_BASE_LIENS;?>/index.php?action=voirPageAjoutRepresentations" method="post">
+						<div>
+							<label>Rechercher par code : </label>
+							<input type="text" name="code_infos"/>
+						</div>
+						<input type="submit" value="Chercher le code infos"/>
+					</form>
+
+
+
 </body>
 
 </html>
