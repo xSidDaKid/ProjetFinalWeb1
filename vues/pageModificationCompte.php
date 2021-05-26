7987613e908639f61d82140e50e74e7cf7d09feb
Shajaan <shajaan_71@hotmail.com>
2021-05-26 17:25:16 -0400

    Modification de la page de modification

--- vues/pageModificationCompte.php
+++ vues/pageModificationCompte.php
@@ -8,7 +8,7 @@
 <html lang="fr">
 
 <head>
-    <title>Projet-test</title>
+    <title>Modification d'un compte</title>
     <?php
 		include(DOSSIER_BASE_INCLUDE."vues/inclusions_html/head.inc.php");
 	?>
@@ -19,29 +19,10 @@
     <?php
 	    include (DOSSIER_BASE_INCLUDE."vues/inclusions_html/menu.inc.php");
 	?>
-    <h1>Voici la page de modification du compte</h1>
+    <h1 class="p-3 display-2">Modification du compte</h1>
     <?php  
         $id = $controleur->getIdUtilisateur();
     ?>
-    <div class="form-group">
-        <form action="<?php echo DOSSIER_BASE_LIENS;?>/index.php?action=voirPageModificationCompte" method="post">
-            <label for="text"><?php echo $id;?></label>
-            <label for="text">Téléphone</label>
-            <input type="text" name="telephone" id="" class="form-control" placeholder="Téléphone"
-                aria-describedby="helpId">
-
-            </br>
-            <label for="text"><?php echo $id;?></label>
-            <label for="text">Solde</label>
-            <input type="text" name="solde" id="" class="form-control" placeholder="Solde"
-                aria-describedby="helpId">
-                </br>
-            <input type="submit" value="Modifier"/>
-                
-        </form>
-        </br>   
-    </div>
-
     <?php
           echo "<div class='bg-success text-center'>";
           afficherSucces($controleur->getMessagesSucces());
@@ -50,8 +31,27 @@
           afficherErreurs($controleur->getMessagesErreur());
           echo "</div>";
     ?>
-   
-     <?php 
+
+    <div class="card centrer w-25 mt-5">
+        <div class="form-group">
+            <label for="text">Votre ID est: [#<?php echo $id;?>]</label>
+            <form action="<?php echo DOSSIER_BASE_LIENS;?>/index.php?action=voirPageModificationCompte" method="post">
+                <div class="form-group">
+                    <label for="text">Téléphone</label>
+                    <input type="text" name="telephone" id="" class="form-control" placeholder="Téléphone"
+                        aria-describedby="helpId" required>
+                </div>
+                <div class="form-group">
+                    <label for="text">Solde</label>
+                    <input type="text" name="solde" id="" class="form-control" placeholder="Solde"
+                        aria-describedby="helpId" required>
+                </div>
+                <input class="btn btn-primary" type="submit" value="Modifier" />
+            </form>
+        </div>
+    </div>
+    <br>
+    <?php 
        $tabAcheteurs = $controleur->getAcheteurs();
        echo "<ul class='list-group'>";
        foreach ($tabAcheteurs as $tab) {
