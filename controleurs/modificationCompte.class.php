aee36d84e90a6c5a6f4b4cc0f9baff571d8175d7
ShishTaoukGainz <louai.louairoueha@gmail.com>
2021-05-26 14:07:52 +0000

    Update modificationCompte.class.php

--- controleurs/modificationCompte.class.php
+++ controleurs/modificationCompte.class.php
@@ -10,20 +10,32 @@ include_once(DOSSIER_BASE_INCLUDE."modele/DAO/AcheteurDAO.class.php");
 
 
 class modificationCompte extends Controleur {
+    private $unAcheteur = null;
 
     // ******************* Constructeur vide
 	public function __construct() {
 		parent::__construct();
 	}
 
+    public function getAcheteurs() {
+        $this->tabAcheteurs = AcheteurDAO::chercherTous();
+        return $this->tabAcheteurs;
+    }
+
     // ******************* Méthode exécuter action
     public function executerAction() {
-       /* if (ISSET($_POST['telephone']) != null) {
-            # code...
-            $unAcheteur=AcheteurDAO::chercher($this->getIdUtilisateur());
-             AcheteurDAO::modifier($unAcheteur->setTelephone(ISSET($_POST['telephone'])));
-         
-        }*/
+        $id=$this->idUtilisateur;
+        $this->unAcheteur = AcheteurDAO::chercher($this->idUtilisateur);
+
+    if (ISSET($_POST['telephone']) and ISSET($_POST['solde'])){
+        $this->unAcheteur->setTelephone($_POST['telephone']);
+        $this->unAcheteur->payerSolde($_POST['solde']);
+       AcheteurDAO::modifier($this->unAcheteur);
+       array_push ($this->messagesSucces,"Modification éffectué");
+    }
+
+
+      
         return "pageModificationCompte";
     }
 }
