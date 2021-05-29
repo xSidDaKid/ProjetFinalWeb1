9114ead289985a8e93954bfdb4d0954821d9c272
ShishTaoukGainz <louai.louairoueha@gmail.com>
2021-05-29 17:49:32 +0000

    Update pageInfoRepresentation.php

--- vues/pageInfoRepresentation.php
+++ vues/pageInfoRepresentation.php
@@ -22,21 +22,31 @@
 	    include (DOSSIER_BASE_INCLUDE."vues/inclusions_html/menu.inc.php");
 	?>
     <h1 class="p-3 display-2">Information pour l'ajout d'un cinéma</h1>
+    <?php
+          echo "<div class='bg-success text-center'>";
+          afficherSucces($controleur->getMessagesSucces());
+          echo "</div>";
+          echo "<div class='bg-danger text-center'>";
+          afficherErreurs($controleur->getMessagesErreur());
+          echo "</div>";
+    ?>
     <?php 
-        $unCinema=null;
-        $unCinema = CinemaDAO::chercherAvecFiltre("WHERE code_infos=".$_POST['code_infos']);
+        $unCinema = $controleur->getCinema();
         echo "<ul class='list-group w-50 '>";
-        foreach ($unCinema as $tab){
-            echo "<li class='list-group-item'>".$tab ."</li>";
+        foreach ($unCinema as $cinema){
+            if ($cinema->getInfos()==$_SESSION["code_infos"]){
+                echo "<li class='list-group-item'>".$cinema ."</li>";
+            }
         }
         echo "</ul>";
+    
     ?>
     <br>
 
+
     <img src="images\info.png" style="float:left" width="100px">
     <div class="card w-25 mt-2 ml-3 p-2">
-        <form class="mon_formulaire"
-            action="<?php echo DOSSIER_BASE_LIENS;?>/index.php?action=voirPageAjoutRepresentations" method="post">
+        <form class="mon_formulaire" action="<?php echo DOSSIER_BASE_LIENS;?>/index.php?action=voirPageAjoutInfoRepresentations" method="post">
             <div class="form-group">
                 <label>Date du film : </label>
                 <input type="date" name="date" required />
@@ -55,6 +65,10 @@
             </div>
             <input class="btn btn-primary" type="submit" value="Créer" />
         </form>
+        <form action="<?php echo DOSSIER_BASE_LIENS;?>/index.php?action=voirPageAjoutRepresentations" method="post">
+            <input class="btn btn-primary" type="submit" name="terminer" value="Terminer" />
+        </form>
+
     </div>
     <?php 
        /*$tabCinema = $controleur->getCinema();
